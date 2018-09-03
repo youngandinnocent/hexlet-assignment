7cc175a3f8f3460c8fcbc1088a8b4f05e8c37dda
Vanek <b.ivan72@mail.ru>
2018-09-03 11:19:43 +0300

    greatest common divisor

---
+++ intro/while_loop.js
@@ -0,0 +1,45 @@
+/*
+ Task:
+ Экспортируйте функцию по умолчанию, которая находит наибольший общий делитель двух целых положительных чисел.
+ Наибольшим общим делителем (НОД) для двух целых чисел m и n называется наибольший из их общих делителей.
+ Пример: для чисел 70 и 105 наибольший общий делитель равен 35. Наибольший общий делитель существует и однозначно
+ определён, если хотя бы одно из чисел m или n не равно нулю.
+*/
+
+// Solution:
+
+//V1.1
+export default (m, n) => {
+  if (m <= 0 && n <= 0) {
+    return NaN;
+  }
+  console.log(m, n);
+  if (m >= n) {
+    console.log(m, n);
+    let result = n;
+    while (m % result !== 0 || n % result !== 0) {
+      result -= 1;
+    }
+    console.log(m, n, result);
+    return result;
+  } else if (m < n) {
+    let result = m;
+    while (m % result !== 0 || n % result !== 0) {
+      result -= 1;
+    }
+    return result;
+  }
+};
+
+//V1.2
+export default (m, n) => {
+  if (m <= 0 && n <= 0) {
+    return NaN;
+  }
+  let result = (m > n) ? m : n;
+
+  while (m % result !== 0 || n % result !== 0) {
+    result -= 1;
+  }
+  return result;
+};

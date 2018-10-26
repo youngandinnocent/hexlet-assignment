cbc1bcb86ac5e2a8a8d068d9bf0989898d4765b5
Vanek <b.ivan72@mail.ru>
2018-10-26 18:24:24 +0300

    Ackermann function

---
+++ intro/ackermannFunction.js
@@ -0,0 +1,18 @@
+// Task: Реализуйте и экспортируйте по умолчанию функцию Аккермана.
+
+// Solution:
+
+const ackermann = (m, n) => {
+  if (m < 0 || n < 0) {
+    return;
+  } else if (m === 0) {
+    return n + 1;
+  } else if (m > 0 && n === 0) {
+    return ackermann(m - 1, 1);
+  } else if (m > 0 && n > 0) {
+    return ackermann(m - 1, ackermann(m, n - 1));
+  }
+};
+export default ackermann;
+
+ackermann(3, 10); // 8189

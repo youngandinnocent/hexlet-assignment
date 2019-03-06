8534b2076fb643e2984db28f7daa317032c9e30e
Vanek <b.ivan72@mail.ru>
2019-03-06 11:17:55 +0300

    linter errors fixed, additional solution added

--- intro/reverseInt.js
+++ intro/reverseInt.js
@@ -2,17 +2,41 @@
 
 // Solution:
 
+
+//V1.1 translation of a number into a string
 const length = str => str.length;
 
-const reverse = (number) => {
-  const str  = String(number);
+const reverseInt = (num) => {
+  const str = String(num);
+
   let result = '';
   let i = (str[0] === '-') ? 1 : 0;
   while (i < length(str)) {
     result = str[i] + result;
     i += 1;
   }
-  return result = (str[0] === '-') ? -Number(result) : Number(result);
+  return (str[0] === '-') ? -Number(result) : Number(result);
 };
 
+export default reverseInt;
+
 reverse(-13786); //-68731
+
+
+
+//V1.2 using the Math method
+const length = str => str.length;
+
+const reverseInt = (num) => {
+  const str = String(Math.abs(num));
+
+  let result = '';
+  let i = str.length - 1;
+  while (i >= 0) {
+    result += str[i];
+    i -= 1;
+  }
+  return num < 0 ? -Number(result) : Number(result);
+};
+
+export default reverseInt;

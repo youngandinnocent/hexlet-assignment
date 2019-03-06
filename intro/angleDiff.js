ba4e528a00b7a35f031ad0f23cf938393bc29ee1
Vanek <b.ivan72@mail.ru>
2019-03-06 14:00:32 +0300

    logical errors corrected, additional solution added

--- intro/angleDiff.js
+++ intro/angleDiff.js
@@ -2,10 +2,26 @@
 
 // Solution:
 
+V1.2 using the Math method
+const diff = (a, b) => {
+  const angle = Math.abs(a - b);
+  return Math.min(angle, 360 - angle);
+};
+export default diff;
+
+
+V1.2 low level solution
 const diff = (angle1, angle2) => {
-  const diff1 = angle2 - angle1;
-  const diff2 = (360 + angle1) - angle2;
-  return (diff1 < diff2 ? diff1 : diff2);
+  let result;
+  if ((angle1 >= 180 && angle2 >= 180) || (angle1 <= 180 && angle2 <= 180)) {
+    result = (angle1 > angle2) ? angle1 - angle2 : angle2 - angle1;
+  } else if (angle1 >= 180 && angle2 <= 180) {
+    result = (angle1 - angle2 < (360 + angle2) - angle1) ? angle1 - angle2 : (360 + angle2) - angle1;
+  } else if (angle1 <= 180 && angle2 >= 180) {
+    result = (angle2 - angle1 < (360 + angle1) - angle2) ? angle2 - angle1 : (360 + angle1) - angle2;
+  }
+  return result;
 };
 
+export default diff;
 diff(0, 190);//170

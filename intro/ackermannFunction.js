0683b45b289833bca03b5c0a3160bafd42328855
Vanek <b.ivan72@mail.ru>
2019-03-08 20:45:26 +0300

    fixed linters

--- intro/ackermannFunction.js
+++ intro/ackermannFunction.js
@@ -3,13 +3,13 @@
 // Solution:
 
 const ackermann = (m, n) => {
-  if (m < 0 || n < 0) {
-    return;
-  } else if (m === 0) {
+  if (m === 0) {
     return n + 1;
-  } else if (m > 0 && n === 0) {
+  }
+  if (m > 0 && n === 0) {
     return ackermann(m - 1, 1);
-  } else if (m > 0 && n > 0) {
+  }
+  if (m > 0 && n > 0) {
     return ackermann(m - 1, ackermann(m, n - 1));
   }
 };

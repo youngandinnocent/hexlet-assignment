77d8cc9b1e3791987b542761be2f5378439b2bb5
Vanek <b.ivan72@mail.ru>
2019-03-08 20:23:25 +0300

    bugs fixed, code cleaned

--- intro/formattadTime.js
+++ intro/formattadTime.js
@@ -5,14 +5,18 @@
 
 // Solution:
 
-const formattadTime = (minutes) => {
-  const day = String(Math.floor(minutes / 1440) + 1);
-  if (minutes > 1440) {
-    minutes = minutes - (Math.floor(minutes / 1440) * 1440);
-  }
-  const hours = (Math.floor(minutes / 60) < 10) ? `0${String(Math.floor(minutes / 60))}` : String(Math.floor(minutes / 60));
-  const min = (minutes - (Math.floor(minutes / 60) * 60) < 10) ? `0${String(minutes - (Math.floor(minutes / 60) * 60))}` : String(minutes - (Math.floor(minutes / 60) * 60));
-  return `${hours}:${min}, day ${day}`;
-}
+const formattedTime = (number) => {
+  const day = Math.floor(number / 1440) + 1;
+
+  const minute = number > 1440 ? number - (Math.floor(number / 1440) * 1440) : number;
+
+  const hours = Math.floor(minute / 60);
+  const formattedHours = hours < 10 ? `0${hours}` : hours;
+
+  const minutes = minute - (Math.floor(minute / 60) * 60);
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+  return `${formattedHours}:${formattedMinutes}, day ${day}`;
+};
 
 formattadTime(7754); // '09:14, day 6'

fd21008e4d79d85c074de5e9c7182ac6d09843d2
vanek <ivan.bermann@gmail.com>
2019-11-17 22:58:12 +0300

    lexer

--- automataBasedProgramming/awkLexer.js
+++ automataBasedProgramming/awkLexer.js
@@ -1,3 +1,24 @@
+/*
+Task:
+*    Реализуйте и экспортируйте функцию по умолчанию, которая принимает
+*    на вход текст и возвращает массив состоящий из первых слов каждой
+*    строки текста. Пустые строчки должны игнорироваться.
+*        1.Строки разделяются переводом строки
+*        2.В любом месте строки может быть сколько угодно пробелов
+*        3.Текст должен перебираться посимвольно (мы пишем лексер)
+*
+*    const text = '  what who   \nhellomy\n hello who are you\n';
+*    const result = solution(text);
+*   [
+*    'what',
+*    'hellomy',
+*    'hello',
+*   ];
+*/
+
+
+// Solution1.0 with array:
+
 export default (text) => {
   const result = [];
   let state = 'before';
@@ -35,3 +56,42 @@ export default (text) => {
 
   return result;
 };
+
+// Solution1.1 with for loop:
+
+const fsm = (str) => {
+  let result = '';
+  let state = 'before';
+  for (let i = 0; i < str.length; i += 1) {
+    const symbol = str[i];
+    switch (state) {
+      case 'before':
+        if (symbol !== ' ' && symbol !== '\n') {
+          result += symbol;
+          state = 'inside';
+        }
+        break;
+      case 'inside':
+        if (symbol !== ' ' && symbol !== '\n') {
+          result += symbol;
+        } else if (symbol === '\n') {
+          result += symbol;
+          state = 'before';
+        } else {
+          state = 'after';
+        }
+        break;
+      case 'after':
+        if (symbol === '\n') {
+          result += symbol;
+          state = 'before';
+        }
+        break;
+      default:
+        return undefined;
+    }
+  }
+  return result;
+};
+  
+export default (str) => fsm(str).split('\n').filter((word) => word !== '');
